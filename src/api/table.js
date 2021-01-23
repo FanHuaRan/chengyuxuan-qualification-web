32d26db620aba51653b23e7e6ab7744b233c786c
fanhuaran <fanhuaran@zhihu.com>
2021-01-23 12:35:32 +0800

    feat: 银行管理页面

--- src/api/table.js
+++ src/api/table.js
@@ -1,9 +1,26 @@
 import request from '@/utils/request'
 
-export function getList(params) {
+export function searchBank(params) {
   return request({
     url: '/bank/search_by_page',
     method: 'get',
     params
   })
 }
+
+export function saveBank(params) {
+  return request({
+    url: '/bank/save',
+    method: 'post',
+    data: params
+  })
+}
+
+export function deleteBank(id) {
+  return request({
+    url: '/bank/delete',
+    method: 'post',
+    params: {"id": id}
+  })
+}
+
--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -6,12 +6,9 @@
       </el-form-item>
       <el-form-item>
         <el-button type="primary" size="small" @click="onSearch">查询</el-button>
+        <el-button type="danger" size="small" @click="onAdd">新增</el-button>
       </el-form-item>
     </el-form>
-
-    <div>
-      <p><el-button type="primary" size="small" class="right">新增</el-button></p>
-    </div>
 
     <el-table
       v-loading="listLoading"
@@ -38,11 +35,14 @@
       </el-table-column>
       <el-table-column label="政策">
         <template slot-scope="scope">
-          {{ scope.row.polices }}
+          <li v-for="value in scope.row.policies">
+            {{ value }}
+          </li>
         </template>
+        i
       </el-table-column>
       <!--      <el-table-column class-name="status-col" label="Status" width="110" align="center">-->
-      <!--        <template slot-scope="scope">-->
+      <!--        <template slot-scope  ="scope">-->
       <!--          <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>-->
       <!--        </template>-->
       <!--      </el-table-column>-->
@@ -58,17 +58,20 @@
       </el-table-column>
       <el-table-column label="操作">
         <template slot-scope="scope">
-          <el-button
-            size="mini"
-            @click="handleEdit(scope.$index, scope.row)">详情</el-button>
+<!--          <el-button-->
+<!--            size="mini"-->
+<!--            @click="handleEdit(scope.$index, scope.row)">详情-->
+<!--          </el-button>-->
           <el-button
             size="mini"
             type="primary"
-            @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+            @click="onEdit(scope.$index, scope.row)">编辑
+          </el-button>
           <el-button
             size="mini"
             type="danger"
-            @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+            @click="handleDelete(scope.$index, scope.row)">删除
+          </el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -78,11 +81,65 @@
       :page-size=pageSize
       :total=total>
     </el-pagination>
+
+    <el-dialog title="新增/编辑" :visible.sync="formVisible">
+      <el-form :model="form">
+        <el-form-item label="银行名称">
+          <el-input v-model="form.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="联系人电话">
+          <el-input v-model="form.phone" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="政策">
+          <i class="el-icon-circle-plus" @click="policeFormVisible=true"></i>
+          <el-table
+            :data="form.policies"
+            border
+            fit
+            highlight-current-row
+          >
+            <el-table-column label="政策条例">
+              <template slot-scope="scope">
+                {{ scope.row }}
+              </template>
+            </el-table-column>
+
+            <el-table-column align="center" label="操作" width="95">
+              <template slot-scope="scope">
+                <i class="el-icon-setting" @click="showPoliceEdit(scope.row, scope.$index)" style="margin-left: 10px"></i>
+                <i class="el-icon-delete-solid" @click="form.policies.remove(scope.$index)" style="margin-left: 10px"></i>
+              </template>
+            </el-table-column>
+          </el-table>
+
+          <el-dialog
+            width="30%"
+            title="编辑政策"
+            :visible.sync="policeFormVisible"
+            append-to-body>
+            <el-input
+              type="textarea"
+              :rows="5"
+              placeholder="请输入内容"
+              v-model="policeForm.policeValue">
+            </el-input>
+            <div slot="footer" class="dialog-footer">
+              <el-button @click="handlePoliceCancel">取 消</el-button>
+              <el-button type="primary" @click="handlePoliceSave">确 定</el-button>
+            </div>
+          </el-dialog>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="handleCancel">取 消</el-button>
+        <el-button type="primary" @click="handleSave">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-  import {getList} from '@/api/table'
+  import {searchBank, saveBank, deleteBank} from '@/api/table'
   import {parseTime} from "@/utils";
 
   export default {
@@ -105,12 +162,26 @@
         total: 20,
         searchForm: {
           name: '',
-        }
+        },
+        form: {
+          id: null,
+          name: null,
+          phone: null,
+          policies: []
+        },
+        formVisible: false,
+        policeForm: {
+          policeValue: null,
+          policeIndex: null,
+        },
+        policeFormVisible: false
       }
     },
+
     created() {
       this.fetchData()
     },
+
     methods: {
       onSearch() {
         this.currentPage = 1
@@ -118,7 +189,7 @@
       },
       fetchData() {
         this.listLoading = true
-        getList({
+        searchBank({
           pageNum: this.currentPage,
           pageSize: this.pageSize,
           name: this.searchForm.name,
@@ -132,12 +203,88 @@
       formatTime(timeStamp) {
         return parseTime(timeStamp, "{y}-{m}-{d} {h}:{i}:{s}")
       },
-      handleEdit(index, row) {
-        console.log(index, row);
-      },
+
       handleDelete(index, row) {
-        console.log(index, row);
-      }
+        this.$confirm('确认删除?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          deleteBank(row.id).then(response => {
+            this.fetchData()
+          })
+        })
+      },
+
+      onAdd(){
+        this.formVisible = true
+        this.form.id = null
+        this.form.name = null
+        this.form.phone = null
+        this.form.policies = []
+      },
+
+      onEdit(index, row) {
+        this.form.id = row.id
+        this.form.name = row.name
+        this.form.phone = row.phone
+        this.form.policies = row.policies
+        this.formVisible = true
+      },
+
+      handleCancel() {
+        this.form.id = null
+        this.form.name = null
+        this.form.phone = null
+        this.form.policies = []
+        this.formVisible = false
+      },
+
+      handleSave() {
+        this.$confirm('确认保存?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          const params = {
+            "id": this.form.id,
+            "name": this.form.name,
+            "phone": this.form.phone,
+            "policies": this.form.policies
+          }
+          saveBank(params).then(response => {
+            this.form.id = null
+            this.form.name = null
+            this.form.phone = null
+            this.form.policies = []
+            this.formVisible = false
+            this.fetchData()
+          })
+        })
+      },
+
+      showPoliceEdit(row, index) {
+        this.policeForm.policeIndex = index
+        this.policeForm.policeValue = row
+        this.policeFormVisible = true
+      },
+
+      handlePoliceSave() {
+        if (this.policeForm.policeIndex !== null) {
+          this.form.policies.splice(this.policeForm.policeIndex, 1, this.policeForm.policeValue)
+        } else {
+          this.form.policies.push(this.policeForm.policeValue)
+        }
+        this.policeFormVisible = false
+        this.policeForm.policeIndex = null
+        this.policeForm.policeValue = null
+      },
+
+      handlePoliceCancel() {
+        this.policeFormVisible = false
+        this.policeForm.policeIndex = null
+        this.policeForm.policeValue = null
+      },
     }
   }
 </script>
